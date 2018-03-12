12a93ed3e3e753a8bccce3ece9bb2f701c44ba41
Abdul Moid <amoid@outlook.com>
2018-03-12 00:10:45 -0500

    Initial project setup.

---
+++ Weather Widget/Weather Widget/Weather_Widget.h
@@ -0,0 +1,19 @@
+//
+//  Weather_Widget.h
+//  Weather Widget
+//
+//  Created by Abdulmoid Mohammed on 3/9/18.
+//  Copyright © 2018 Abdul-Moid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Weather_Widget.
+FOUNDATION_EXPORT double Weather_WidgetVersionNumber;
+
+//! Project version string for Weather_Widget.
+FOUNDATION_EXPORT const unsigned char Weather_WidgetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Weather_Widget/PublicHeader.h>
+
+
